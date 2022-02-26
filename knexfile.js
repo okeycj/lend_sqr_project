573cecea259a46eeb1f24deb78974afc7d7bfec4
chijindu okechukwu <okechukwu008@gmail.com>
2022-02-26 20:58:43 +0100

    Slight fix

--- knexfile.js
+++ knexfile.js
@@ -8,13 +8,7 @@ module.exports = {
   development: {
     client: 'mysql',
     version: '5.7',
-    connection: {
-      host : process.env.MYSQL_HOST,
-      port : process.env.MYSQL_PORT,
-      user : process.env.MYSQL_USERNAME,
-      password : process.env.MYSQL_PASSWORD,
-      database : process.env.MYSQL_DATABASE
-    }
+    connection: process.env.CLEARDB_DATABASE_URL
   },
   
   production: {
